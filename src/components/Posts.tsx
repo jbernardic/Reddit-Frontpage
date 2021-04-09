a30918f09ada56332a6a5180156b597e52f275e4
janxy-dev <47927018+janxy-dev@users.noreply.github.com>
2021-04-10 00:03:37 +0200

    Updated filter box

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -5,10 +5,11 @@ const Posts = () =>{
         <div className={styles.posts}>
             <h4>Popular posts</h4>
             <div className={styles.filterBox}>
-                <button className={styles.hot}><i className="fas fa-fire"></i>Hot</button>
-                <button className={styles.country}>Croatia<i className="fas fa-caret-down"></i></button>
+                <button className={`${styles.o}`}><i className="fas fa-fire"></i>Hot</button>
+                <button className={`${styles.dropDown} ${styles.o}`}>Croatia<i className="fas fa-caret-down"></i></button>
                 <button className={styles.new}><i className="fas fa-certificate"></i>New</button>
-                <button className={styles.top}><i className="fas fa-chart-line"></i>Top</button>
+                <button className={`${styles.o}`}><i className="fas fa-chart-line"></i>Top</button>
+                <button className={`${styles.dropDown} ${styles.o}`}>Today<i className="fas fa-caret-down"></i></button>
             </div>
         </div>
     );

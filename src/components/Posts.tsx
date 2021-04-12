9efda39fa725dc121757ec02211724cafe327b42
janxy-dev <47927018+janxy-dev@users.noreply.github.com>
2021-04-12 14:34:39 +0200

    implemented auto posts update on scroll

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -4,18 +4,34 @@ import PostFilter from './PostFilter'
 import {useEffect, useState} from 'react'
 import {getPosts} from './api/redditapi'
 
+let postsUpdated = 25;
+
 const Posts = () =>{
     const [posts, setPosts] = useState<any[]>([]);
-    useEffect(() =>{
-        getPosts("popular", 25).then(data => data.map((item:any, idx:number) =>{
+
+    function updatePosts(amount: number){
+        getPosts("popular", amount).then(data => data.map((item:any, idx:number) =>{
             let text:string = item.data.selftext;
             let textLength = text.length;
-            text = text.slice(1, 1000);
-            if(textLength > 1000) text+="...";
+            text = text.slice(0, 700);
+            if(textLength > 700) text+="...";
             return (<Post title={item.data.title} body={text}
             author={item.data.author} subreddit={item.data.subreddit} time={item.data.created_utc} score={item.data.score}
             num_comments={item.data.num_comments} key={idx}/>);
             })).then(data => {setPosts(data);});
+    }
+
+    useEffect(() =>{
+        updatePosts(postsUpdated);
+        postsUpdated+=25;
+        document.addEventListener("scroll", () =>{
+            let maxScroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            if(window.scrollY > maxScroll-500){
+                window.scroll(0, maxScroll-500);
+                updatePosts(postsUpdated);
+                postsUpdated+=25;
+            }
+        })
     }, []);
 
     return(

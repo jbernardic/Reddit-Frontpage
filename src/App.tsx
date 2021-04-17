e59c0122c8725ddd93f8abf8714da9d51f55ea71
janxy-dev <47927018+janxy-dev@users.noreply.github.com>
2021-04-17 19:50:10 +0200

    remove sidebar on width <960

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
+  window.scrollTo(0, 0);
   return (
     <>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" />

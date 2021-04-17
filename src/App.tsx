1af26661a18dfe6ebb99adacb4d93fe4c0b265a6
janxy-dev <47927018+janxy-dev@users.noreply.github.com>
2021-04-17 15:09:47 +0200

    updated fonts

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" />
-      <Navbar/>
+    <Navbar/>
     <div className="content">
       <Trending/>
       <div className="grid">
